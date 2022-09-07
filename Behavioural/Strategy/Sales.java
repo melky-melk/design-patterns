1bb11b75ee40cfb3113238a6d7e29ac689e6657f
melk <ctan6841@uni.sydney.edu.au>
2022-09-07 21:52:22 +1000

    commenting over the sales.java in strategy

--- Behavioural/Strategy/Sales.java
+++ Behavioural/Strategy/Sales.java
@@ -5,6 +5,7 @@ interface SalesPricing{
 class SalePricingContext{
 	private SalePricing salePricing;
 
+	// sets the salePricing to know what method it is going to use
 	public SalePricingContext(SalePricing salePricing){
 		this.salePricing = salePricing;
 	}
@@ -16,34 +17,35 @@ class SalePricingContext{
 }
 
 class SalePricingNormal implements SalePricing{
-	public double getTotal(int quantity, double price){
-		return quantity * price;
-	}
+
+	// gets the total like normally
+	public double getTotal(int quantity, double price)	{return quantity * price;}
 }
 
 class SalePricingWithDiscount implements SalePricing{
 	private double discount = 0.0;
 
-	public double getTotal(int quantity, double price){
-		return quantity * price * discount;
-	}
+	// does the same get total but with a discound
+	public double getTotal(int quantity, double price)	{return quantity * price * discount;}
 }
 
 class SalePricingWithCashBack implements SalePricing{
 	private double discount = 0.0;
 	private double threshold = 0.0;
 
-	public double getTotal(int quantity, double price){
-		return quantity * price * discount;
-	}
+	// does the discount calculation but has a threshold it cant go over
+	public double getTotal(int quantity, double price)	{return quantity * price * discount;}
 }
 
 public class Sales{
 	public static void main(String[] args){
+		// first creates a sale pricing, with the specific kind of sale pricing afterwards
 		SalePricing salePricing = new SalePricingNormal();
 
+		// then making the context using the specific salePricing set
 		SalePricingContext context = new SalePricingContext(salePricing);
 
+		// this will use the normal sale pricing
 		double total = context.getTotal(6, 4);
 		System.out.println("The total money you need to pay is " + total);
 	}
