b7283e312a401f0b8a76aefe3de0969144422f32
melk <ctan6841@uni.sydney.edu.au>
2022-09-07 21:40:58 +1000

    adding SocialAccountBuilder

--- Creational/Builder/SocialAccount.java
+++ Creational/Builder/SocialAccount.java
@@ -1,1 +1,134 @@
 // melk find the account stuff you did last year
+class SocialAccount{
+
+	private String firstName;
+	private String lastName;
+	private String location;
+	private String phoneNumber;
+	private String email;
+	private String profileURl;
+	private String bio;
+	private int age;
+	private SocialAccount[] importedFriends;
+
+	public SocialAccountBuilder(String firstName, String lastName){
+		assert firstName != null && lastName != null;
+		
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	// returns the social account so you can stack method calls on top 
+	public SocialAccount setAge(int age){
+		this.age = age;
+		return this;
+	}
+
+	public SocialAccount setFirstName(String firstName) {
+		this.firstName = firstName;
+		return this;
+	}
+
+	public SocialAccount setLastName(String lastName) {
+		this.lastName = lastName;
+		return this;
+	}
+
+	public SocialAccount setLocation(String location) {
+		this.location = location;
+		return this;
+	}
+
+	public SocialAccount setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+		return this;
+	}
+
+	public SocialAccount setEmail(String email) {
+		this.email = email;
+		return this;
+	}
+
+	public SocialAccount setProfileURl(String profileURl) {
+		this.profileURl = profileURl;
+		return this;
+	}
+
+	public SocialAccount setBio(String bio) {
+		this.bio = bio;
+		return this;
+	}
+}
+
+
+public class SocialAccount {
+
+	private String firstName;
+	private String lastName;
+	private String location;
+	private String phoneNumber;
+	private String email;
+	private String profileURl;
+	private String bio;
+	private int age;
+	private SocialAccount[] importedFriends;
+
+
+	public SocialAccount(String firstName, String lastName, int age, String location, String phoneNumber, String email, String profileURl, String bio, SocialAccount[] importedFriends){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.location = location;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.profileURl = profileURl;
+		this.bio = bio;
+		this.age = age;
+		this.importedFriends = importedFriends;
+	}
+
+	// public SocialAccount(String firstName, String lastName){
+	// 	this.firstName = firstName;
+	// 	this.lastName = lastName;
+	// }
+
+	// public SocialAccount(String firstName, String lastName, int age){
+	// 	this.firstName = firstName;
+	// 	this.lastName = lastName;
+	// 	this.age = age;
+	// }
+
+	//DOESNT WORK BECAUSE THERE ARE 2 STRING PARAMETER OVERLOAD
+	/*
+	public SocialAccount(String firstName, String lastName, String phoneNumber){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+	}
+	public SocialAccount(String firstName, String lastName, String email){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+	*/
+
+	public static void main(String[] args){
+		SocialAccount alex = new SocialAccount("Alex", "Smith", 21, null, null, null, null, "www.profile.com", "");
+		SocialAccount bill = new SocialAccount("Bill", "Damon", 19, null, "314159265", null, null, "Small bio");
+		SocialAccount carl = new SocialAccount("Carl", "Grimes", 16, "Georgia", null, null, null, null);
+
+		SocialAccount gregs = new SocialAccount("GREG", "GREG", 0, null, null, null, null, null);
+		SocialAccount marijuanaBuilder = new SocialAccountBuilder("Mary", "Jane").build();
+		// marijuanaBuilder.setAge(20);
+		// marijuanaBuilder.setEmail("mary@gmail.com");
+		// SocialAccount mary = marijuanaBuilder.builder();
+
+		// REFACTOR APP USING THIS
+		SocialAccount greg = new SocialAccountBuilder("GREG!", "GREG!").setAge(20)
+																	   .setEmail("greg!@gmail.com")
+																	   .build();
+
+		SocialAccount greg = new SocialAccountBuilder("GREG!", "GREG!").setAge(20).setEmail("greg!@gmail.com").build();
+		
+		
+	}
+}
---
+++ Creational/Builder/SocialAccountBuilder.java
@@ -0,0 +1,86 @@
+public class SocialAccount {
+	private String firstName, lastName, location, phoneNumber, email, profileURl, bio;
+	private int age;
+	private SocialAccount[] importedFriends;
+
+	public SocialAccount(String firstName, String lastName, int age, String location, String phoneNumber, String email, String profileURl, String bio, SocialAccount[] importedFriends){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.location = location;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.profileURl = profileURl;
+		this.bio = bio;
+		this.age = age;
+		this.importedFriends = importedFriends;
+	}
+}
+public class SocialAccountBuilder{
+	private String firstName, lastName, location, phoneNumber, email, profileURl, bio;
+	private int age;
+	private SocialAccount[] importedFriends;
+
+	public SocialAccountBuilder(String firstName, String lastName){
+		assert firstName != null && lastName != null;
+		
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	// returns the social account so you can stack method calls on top 
+	public SocialAccountBuilder setAge(int age)						{
+		this.age = age;
+		return this;
+	}
+
+	public SocialAccountBuilder setFirstName(String firstName) 		{
+		this.firstName = firstName;
+		return this;
+	}
+
+	public SocialAccountBuilder setLastName(String lastName) 		{
+		this.lastName = lastName;
+		return this;
+	}
+
+	public SocialAccountBuilder setLocation(String location) 		{
+		this.location = location;
+		return this;
+	}
+
+	public SocialAccountBuilder setPhoneNumber(String phoneNumber) 	{
+		this.phoneNumber = phoneNumber;
+		return this;
+	}
+
+	public SocialAccountBuilder setEmail(String email) 				{
+		this.email = email;
+		return this;
+	}
+
+	public SocialAccountBuilder setProfileURl(String profileURl) 	{
+		this.profileURl = profileURl;
+		return this;
+	}
+
+	public SocialAccountBuilder setBio(String bio) 					{
+		this.bio = bio;
+		return this;
+	}
+	
+	public SocialAccount build(){
+		// anything that hasnt been initialised will be made into null
+		return new SocialAccount(firstName, lastName, location, phoneNumber, email, profileURl, bio, age, importedFriends);
+	}
+
+	public static void main(String[] args){
+		// without the builder it  would be a pain to have all of the constructors like this in order as well
+		SocialAccount bill = new SocialAccount("Bill", "Damon", 19, null, "314159265", null, null, "Small bio");
+		
+		//with the builder it can add everything it needs to without having to memorise where it goes in the constructor
+		SocialAccount greg = new SocialAccountBuilder("GREG!", "GREG!")
+		.setAge(20)
+		.setEmail("greg!@gmail.com")
+		.build();
+	}
+}
